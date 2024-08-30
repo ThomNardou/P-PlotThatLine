5e18d45959ae6fcd39f31ae0efca590bab52fd38
Tytan <thomasnardou@icloud.com>
2024-08-30 15:57:54 +0200

    Update (Readme): add source link in the readme file

--- P-WorldPopulationApp/P-WorldPopulationApp/Form1.cs
+++ P-WorldPopulationApp/P-WorldPopulationApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace P_WorldPopulationApp
 {
@@ -15,6 +16,27 @@ namespace P_WorldPopulationApp
         public Form1()
         {
             InitializeComponent();
+            Series s = new Series("test");
+            s.ChartType = SeriesChartType.Line;
+            this.worldpop.Series.Add(s);
+
+            this.worldpop.Series[s.Name].Points.AddXY("Test1", 28);
+            this.worldpop.Series[s.Name].Points.AddXY("Test2", 63);
+            this.worldpop.Series[s.Name].Points.AddXY("Test3", 50);
+            this.worldpop.Series[s.Name].Points.AddXY("Test4", 20);
+            this.worldpop.Series[s.Name].Points.AddXY("Test5", 74);
+            this.worldpop.Series[s.Name].Points.AddXY("Test6", 12);
+            this.worldpop.Series[s.Name].Points.AddXY("Test7", 58);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # P-PlotThatLine
-Plot That Line is a C# application who display world population evolution 
+Plot That Line is a graphical C# application who display world population evolution 
+
+<img src="./imageReadmeWP.jpeg">
+
+## Sources :
+- [Datas](https://www.kaggle.com/datasets/iamsouravbanerjee/world-population-dataset?resource=download)
+- [How to use Chart in Visual C#](https://www.youtube.com/watch?v=82jnryBxsnI)
+
+
+## Project Managment
+This project has been manage with [IceScrum](https://www.icescrum.com/fr/) 
+
+Project link [here](https://etml.icescrum.com/p/PSTATISTIC/#/project)
